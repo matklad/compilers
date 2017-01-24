b169ea46ffe9ffec69dcf924033e340604ece50d
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-01-24 23:37:52 +0300

    Add AstElement trait

--- stc/src/ast.rs
+++ stc/src/ast.rs
@@ -1,4 +1,4 @@
-use syntax::{TokenFile, RstFile, Node};
+use syntax::{TokenFile, RstFile, Node, NodeType};
 
 use rst::{tiny_tokenizer, tiny_parser, LITERAL, ID, LIST, LPAREN, RPAREN, TINY_FILE};
 
@@ -18,12 +18,29 @@ impl AstFile {
     }
 
     pub fn root(&self) -> Program {
-        Program { node: self.rst.root() }
+        Program::from_node(self.rst.root())
     }
+}
+
+trait AstElement<'f> {
+    fn ty() -> NodeType;
+
+    fn from_node(node: Node<'f>) -> Self;
 }
 
 pub struct Program<'f> {
     node: Node<'f>
+}
+
+impl<'f> AstElement<'f> for Program<'f> {
+    fn ty() -> NodeType {
+        TINY_FILE
+    }
+
+    fn from_node(node: Node<'f>) -> Self {
+        assert_eq!(node.ty(), Self::ty());
+        Program { node: node }
+    }
 }
 
 impl<'f> Program<'f> {
@@ -37,6 +54,18 @@ impl<'f> Program<'f> {
 pub struct List<'f> {
     node: Node<'f>
 }
+
+impl<'f> AstElement<'f> for List<'f> {
+    fn ty() -> NodeType {
+        LIST
+    }
+
+    fn from_node(node: Node<'f>) -> Self {
+        assert_eq!(node.ty(), Self::ty());
+        List { node: node }
+    }
+}
+
 
 impl<'f> List<'f> {
     pub fn elements(&self) -> Vec<ListElement> {
@@ -61,10 +90,35 @@ pub struct Literal<'f> {
     node: Node<'f>
 }
 
+impl<'f> AstElement<'f> for Literal<'f> {
+    fn ty() -> NodeType {
+        LITERAL
+    }
+
+    fn from_node(node: Node<'f>) -> Self {
+        assert_eq!(node.ty(), Self::ty());
+        Literal { node: node }
+    }
+}
+
+
 #[derive(Debug)]
 pub struct Variable<'f> {
     node: Node<'f>
 }
+
+
+impl<'f> AstElement<'f> for Variable<'f> {
+    fn ty() -> NodeType {
+        ID
+    }
+
+    fn from_node(node: Node<'f>) -> Self {
+        assert_eq!(node.ty(), Self::ty());
+        Variable { node: node }
+    }
+}
+
 
 #[derive(Debug)]
 pub enum ListElement<'f> {
@@ -76,9 +130,9 @@ pub enum ListElement<'f> {
 impl<'f> ListElement<'f> {
     fn from_node(node: Node) -> ListElement {
         match node.ty() {
-            LITERAL => ListElement::Literal(Literal { node: node }),
-            ID => ListElement::Variable(Variable { node: node }),
-            LIST => ListElement::List(List { node: node }),
+            LITERAL => ListElement::Literal(Literal::from_node(node)),
+            ID => ListElement::Variable(Variable::from_node(node)),
+            LIST => ListElement::List(List::from_node(node)),
             _ => panic!("Bad list node {:?}", node)
         }
     }
