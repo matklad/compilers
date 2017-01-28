06cd1a6dfaf6accfbf96fc1c40afab8058b0e5d9
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-01-28 13:11:40 +0300

    Cleanup

--- pyt/runtime/build.rs
+++ pyt/runtime/build.rs
@@ -5,12 +5,7 @@ use std::path::Path;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    // note that there are a number of downsides to this approach, the comments
-    // below detail how to improve the portability of these commands.
-//    Command::new("gcc").args(&["src/hello.c", "-c", "-fPIC", "-o"])
-//        .arg(&format!("{}/hello.o", out_dir))
-//        .status().unwrap();
-    Command::new("nasm").args(&["src/syscall.asm",  "-felf64", "-o"])
+    Command::new("nasm").args(&["src/syscall.asm", "-felf64", "-o"])
         .arg(&format!("{}/syscall.o", out_dir))
         .status().unwrap();
     Command::new("ar").args(&["crus", "libsyscall.a", "syscall.o"])
